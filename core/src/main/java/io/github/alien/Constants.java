6ea92f453330ecb9e72eab3871ca2da77ab7a81e
Weeersix <lazerkruser132@gmail.com>
2026-09-09 21:25:26 +0300

    some minor changes2

--- core/src/main/java/io/github/alien/Constants.java
+++ core/src/main/java/io/github/alien/Constants.java
@@ -7,9 +7,6 @@ import java.nio.file.Path;
 public class Constants {
     public static final Path ASSETS_FILE_PATH = Path.of("assets/assets.txt");
 
-    public static final int TILE_SIZE = 1;
-    public static final int BASE_SPRITE_SIZE = 16;
-
     public static final float PIXEL_SIZE = 0.0625F;
 
     public static final String NULL = "other/null.png";
